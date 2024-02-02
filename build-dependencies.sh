35e774c3b4c828332c31d850f7d98be6b6afd296
Akash <mailmeakashhalder@gmail.com>
2024-02-02 12:11:04 +0530

    build

--- build-dependencies.sh
+++ build-dependencies.sh
@@ -3,19 +3,20 @@
 set -exu
 set -o pipefail
 
+# Update and install build-essential for c++
 sudo apt update
 sudo apt-get install build-essential
 
 # Check if go is installed
 if ! command -v go &> /dev/null; then
-    echo "Error: go is not installed. Please install Go first."
+    echo "Error: go is not installed. Please install Go 1.21 first."
     exit 1
 fi
 
 go version
 
 # Check fo version is greater than 1.20
-MIN_GO_VERSION="1.20"  # adjust this to your requirements
+MIN_GO_VERSION="1.21"  # adjust this to your requirements
 GO_VERSION=$(go version | awk '{print $3}' | tr -d "go")
 if [[ $(echo "$MIN_GO_VERSION $GO_VERSION" | tr " " "\n" | sort -V | head -n 1) != "$MIN_GO_VERSION" ]]; then
     echo "Error: Go version $GO_VERSION is installed, but version $MIN_GO_VERSION or higher is required."
