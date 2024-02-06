21684b8a9f1e3114f902d9e3a14c7427d5cf1d4d
Akash <mailmeakashhalder@gmail.com>
2024-02-06 10:38:22 +0530

    update sub module

--- build-dependencies.sh
+++ build-dependencies.sh
@@ -25,7 +25,7 @@ if [[ $(echo "$MIN_GO_VERSION $GO_VERSION" | tr " " "\n" | sort -V | head -n 1) 
 fi
 
 #init submodules
-git submodule update --force --recursive --init --remote
+# git submodule update --init --recursive
 
 PRYSM_DIR=./dependencies/prysm
 GETH_DIR=./dependencies/go-ethereum
