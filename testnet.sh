90b61189b99bd8bcc26a947af4c91ab17cacd570
Akash <mailmeakashhalder@gmail.com>
2024-02-07 12:11:04 +0530

    port

--- testnet.sh
+++ testnet.sh
@@ -14,17 +14,17 @@ NUM_NODES=64
 # with more validators to prevent port conflicts on a single machine
 GETH_BOOTNODE_PORT=30301
 
-GETH_HTTP_PORT=8000
-GETH_WS_PORT=8100
-GETH_AUTH_RPC_PORT=8200
-GETH_METRICS_PORT=8300
-GETH_NETWORK_PORT=8400
+GETH_HTTP_PORT=8545
+GETH_WS_PORT=8546
+GETH_AUTH_RPC_PORT=8547
+GETH_METRICS_PORT=8548
+GETH_NETWORK_PORT=8549
 
-PRYSM_BEACON_RPC_PORT=4000
-PRYSM_BEACON_GRPC_GATEWAY_PORT=4100
-PRYSM_BEACON_P2P_TCP_PORT=4200
-PRYSM_BEACON_P2P_UDP_PORT=4300
-PRYSM_BEACON_MONITORING_PORT=4400
+PRYSM_BEACON_RPC_PORT=5000
+PRYSM_BEACON_GRPC_GATEWAY_PORT=5100
+PRYSM_BEACON_P2P_TCP_PORT=5200
+PRYSM_BEACON_P2P_UDP_PORT=5300
+PRYSM_BEACON_MONITORING_PORT=5400
 
 PRYSM_VALIDATOR_RPC_PORT=7000
 PRYSM_VALIDATOR_GRPC_GATEWAY_PORT=7100
@@ -124,10 +124,35 @@ $GETH_BINARY \
 
 sleep 5
 
+# # Start prysm consensus client for this node
+# $PRYSM_BEACON_BINARY \
+#       --datadir=$NODE_DIR/consensus/beacondata \
+#       --min-sync-peers=0 \
+#       --genesis-state=$NODE_DIR/consensus/genesis.ssz \
+#       --bootstrap-node=$PRYSM_BOOTSTRAP_NODE \
+#       --interop-eth1data-votes \
+#       --chain-config-file=$NODE_DIR/consensus/config.yml \
+#       --contract-deployment-block=0 \
+#       --chain-id=${CHAIN_ID:-32382} \
+#       --rpc-host=0.0.0.0 \
+#       --grpc-gateway-host=0.0.0.0 \
+#       --execution-endpoint=http://0.0.0.0:8551 \
+#       --accept-terms-of-use \
+#       --jwt-secret=$NODE_DIR/execution/jwtsecret \
+#       --suggested-fee-recipient=0x123463a4b065722e99115d6c222f267d9cabb524 \
+#       --minimum-peers-per-subnet=0 \
+#       --enable-debug-rpc-endpoints \
+#       --p2p-host-ip=20.244.97.158 \
+#       --minimum-peers-per-subnet=0 \
+#       --monitoring-port=$PRYSM_BEACON_MONITORING_PORT \
+#       --verbosity=info \
+#       --slasher \
+#       --enable-debug-rpc-endpoints > "$NODE_DIR/logs/beacon.log" 2>&1 &
+
 # Start prysm consensus client for this node
 $PRYSM_BEACON_BINARY \
       --datadir=$NODE_DIR/consensus/beacondata \
-      --min-sync-peers=0 \
+      --min-sync-peers=1 \
       --genesis-state=$NODE_DIR/consensus/genesis.ssz \
       --bootstrap-node=$PRYSM_BOOTSTRAP_NODE \
       --interop-eth1data-votes \
@@ -147,6 +172,7 @@ $PRYSM_BEACON_BINARY \
       --monitoring-port=$PRYSM_BEACON_MONITORING_PORT \
       --verbosity=info \
       --slasher \
+      --peer=/ip4/4.240.105.79/tcp/13000/p2p/16Uiu2HAmPMN8mvCrZSKHpqP35GNaw6MfKmL9XoRvd6ekFBsz6o36 \
       --enable-debug-rpc-endpoints > "$NODE_DIR/logs/beacon.log" 2>&1 &
 
 # Start prysm validator for this node. Each validator node will manage 1 validator
